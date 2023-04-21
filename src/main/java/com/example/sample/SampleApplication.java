e3f346b4916e154fb9240bec9277ca5048b089c6
Yohanu Javvadi <yohjavva@publicisgroupe.net>
2023-04-21 12:31:32 +0530

    adding project

---
+++ src/main/java/com/example/sample/SampleApplication.java
@@ -0,0 +1,35 @@
+package com.example.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@SpringBootApplication
+@RestController
+public class SampleApplication {
+
+	Map<String, String> numbers = Map.of("1", "One", "2", "Two", "3", "Three");
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String health() {
+		return "Sample Application is Up and Running!";
+	}
+
+	@GetMapping("/numbers")
+	public Map<String, String> message() {
+		return numbers;
+	}
+
+	@GetMapping("/numbers/{number}")
+	public String getNumber(@PathVariable("number") String number) {
+		return numbers.get(number);
+	}
+}
